8f8242416a2068eb79453ec7209489f777a0387d
zhanghui <batboy2285@126.com>
2017-07-21 15:16:00 +0800

    first commit

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author zhanghui
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello my git of gitHub!");
+	}
+
+}
