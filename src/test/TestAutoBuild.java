48fbaaea668864d152110106525412e0825f5479
zhanghui <batboy2285@126.com>
2017-07-21 15:53:06 +0800

    test auto build
    Signed-off-by: zhanghui <batboy2285@126.com>

---
+++ src/test/TestAutoBuild.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author zhanghui
+ *
+ */
+public class TestAutoBuild {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("test auto build!");
+
+	}
+
+}
